bce70b24f349016fb422258ca3a41a23717c93e7
LiadOlier <liad.olier@campus.technion.ac.il>
2024-08-16 21:20:26 +0300

    connection and initial plot

---
+++ read_from_armband.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mindrove.board_shim import BoardShim, MindRoveInputParams, BoardIds, MindroveConfigMode
+from mindrove.data_filter import DataFilter, FilterTypes, DetrendOperations
+import time
+import armband_functions as funcs
+import plot_live as EMGPlot
+from matplotlib.animation import FuncAnimation
+
+start_time = time.time()
+elapsed_time = time.time()-start_time
+stop_time = 20
+
+board_shim = funcs.connection() # connecting to the armband
+
+emg_channels = BoardShim.get_emg_channels(board_shim.board_id)
+sampling_rate = BoardShim.get_sampling_rate(board_shim.board_id)
+
+window_size = 1 # seconds
+num_points = window_size * sampling_rate
+chanels_num = 8
+
+emg_data = [[] for _ in range(chanels_num)]
+unfiltered_emg_data = [[] for _ in range(chanels_num)]
+
+samples = np.array([])
+
+
+plt.figure()
+
+print(board_shim.is_prepared())
+while (elapsed_time <= stop_time+1):
+  elapsed_time = time.time() - start_time
+
+  if board_shim.get_board_data_count() >= num_points:
+    funcs.get_new_data(unfiltered_emg_data,emg_data,emg_channels,num_points,board_shim,sampling_rate)
+
+    for j in range(chanels_num):
+      plt.subplot(4, 2, j + 1)
+      samples = np.linspace(1, len(emg_data[0]), len(emg_data[0]))
+      plt.plot(samples, emg_data[j])
+      plt.xlim(0, len(samples))
+
+    plt.tight_layout()
+    plt.show()
